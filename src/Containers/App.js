cdb9cc225bd0c81936d08c35d9c6a4e7e91741b9
Gavin Look <GTLook@gmail.com>
2018-10-11 12:55:59 -0700

    stable app

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import { request, AuthenticationService } from '../Helpers'
+
+//import Home from './Home'
+
+//CSS
+import '../CSS/App.css';
 
 class App extends Component {
-  render() {
+  render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Switch>
+              {
+                //<Route exact path='/' component={ Home } />
+              }
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch)
+export default connect(null, mapDispatchToProps)(App)
